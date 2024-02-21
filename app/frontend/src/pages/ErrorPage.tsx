9cadc5a748069c8347140ee0cfefa435d19f10ee
Jerome-Pascal Habanz <jerome-pascal.habanz@mni.thm.de>
2024-02-21 15:44:03 +0100

    ADDED Pipeline and setup instructions in README.md

--- app/frontend/src/pages/ErrorPage.tsx
+++ app/frontend/src/pages/ErrorPage.tsx
@@ -1,18 +1,19 @@
-import { useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
-    const error: any = useRouteError();
-    console.error(error);
+    const error = useRouteError();
 
-    return (
-        <div className="flex h-screen w-screen items-center justify-center text-white">
-            <div className="text-center">
-                <h1 className="text-3xl">Oops!</h1>
-                <p>Sorry, an unexpected error has occurred.</p>
-                <p>
-                    <i>{error.statusText || error.message}</i>
-                </p>
+    if (isRouteErrorResponse(error)) {
+        return (
+            <div className="flex h-screen w-screen items-center justify-center text-white">
+                <div className="text-center">
+                    <h1 className="text-3xl">Oops!</h1>
+                    <p>Sorry, an unexpected error has occurred.</p>
+                    <p>
+                        <i>{error.statusText}</i>
+                    </p>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }

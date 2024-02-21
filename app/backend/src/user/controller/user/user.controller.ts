a2db774da93aa394a661b3a3ba9e29443e697f29
Eike Torben Menzel <eike.torben.menzel@mni.thm.de>
2024-02-21 13:16:36 +0100

    ADDED IsAdminGuard, REFACTORED Profile Routes,

--- app/backend/src/user/controller/user/user.controller.ts
+++ app/backend/src/user/controller/user/user.controller.ts
@@ -1,31 +1,50 @@
-import {Body, Controller, Get, HttpCode, HttpStatus, NotFoundException, Post, Request} from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    NotFoundException,
+    Param, ParseIntPipe,
+    Post,
+    Request,
+    UseGuards
+} from '@nestjs/common';
 import {UserDTO} from "../../payload/UserDTO";
 import {UserService} from "../../services/user/user.service";
 import {UpdateUsernameDTO} from "../../payload/UpdateUsernameDTO";
 import {UserEntity} from "../../../database/models/UserEntity";
+import {IsAdminGuard} from "../../guard/is-admin/is-admin.guard";
 
-@Controller('api/v1/account')
+@Controller('api/v1/profiles')
 export class UserController {
     constructor(
         private userService: UserService
     ) {}
 
-    @Get()
+    @Get('own')
     @HttpCode(HttpStatus.OK)
     async getOwnProfile(@Request() req): Promise<UserDTO> {
-        const result: UserDTO | undefined = await this.userService.transformUserIdToUserDTO(req.user.userId);
-        if(!result) {
-            throw new NotFoundException("User was not found");
-        }
-        return result;
+       return this.getProfile(req.user.userId);
     }
 
-    @Post()
+    @Post('own')
     @HttpCode(HttpStatus.NO_CONTENT)
     async updateOwnProfileUsername(@Request() req, @Body() updateUsernameDTO: UpdateUsernameDTO): Promise<void> {
         const result: UserEntity | undefined = await this.userService.updateUserName(req.user.userId, updateUsernameDTO.username);
         if(!result) {
             throw new NotFoundException("User was not found");
         }
+    }
+
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(IsAdminGuard)
+    async getProfile(@Param("id", ParseIntPipe) requestedId: number): Promise<UserDTO> {
+        const result: UserDTO | undefined = await this.userService.transformUserIdToUserDTO(requestedId);
+        if(!result) {
+            throw new NotFoundException("User was not found");
+        }
+        return result;
     }
 }
---
+++ app/backend/src/user/guard/is-admin/is-admin.guard.ts
@@ -0,0 +1,18 @@
+import {CanActivate, ExecutionContext, ForbiddenException, Injectable} from '@nestjs/common';
+import {UserService} from "../../../database/services/user/user.service";
+
+@Injectable()
+export class IsAdminGuard implements CanActivate {
+    constructor(private userServiceDatabase: UserService) {
+    }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
+        if (!user || !await this.userServiceDatabase.isUserAdmin(user.id)) {
+            throw new ForbiddenException('You need admin privileges to access this route');
+        }
+
+        return true;
+    }
+}
